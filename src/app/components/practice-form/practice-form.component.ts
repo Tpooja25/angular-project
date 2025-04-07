0fead99230e712ebf844aa113fbd1c5af5a826f7
Tpooja25 <tumulapooja123@gmail.com>
2025-04-07 18:44:20 +0530

    changes in form

--- src/app/components/practice-form/practice-form.component.ts
+++ src/app/components/practice-form/practice-form.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormArray } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { retry } from 'rxjs';
 
 @Component({
   selector: 'app-practice-form',
@@ -17,10 +19,26 @@ export class PracticeFormComponent {
 
  constructor(private formbuilder:FormBuilder){
  this.registrationForm = this.formbuilder.group({
-  firstname:['',[Validators.required]]
+  firstname:['',[Validators.required]],
+  lastname:['',[Validators.required]],
+  detailsArray:this.formbuilder.array([])
  })
  }
+ detailsArray():FormArray{
+  return this.registrationForm.get('detailsArray') as FormArray
+ }
 
+ addDetails(){
+ const details = this.formbuilder.group({
+  email:['',[Validators.required,Validators.email]],
+  phnno:['',[Validators.required,Validators.maxLength(10)]],
+  address:['',[Validators.required]],
+ })
+ this.detailsArray().push(details)
+ }
+ delete(index){
+  this.detailsArray().removeAt(index)
+ }
  submit(){
   this.submitted = true;
   if(this.registrationForm.valid){
